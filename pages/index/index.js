75f7fade87f1ea543be698dc4eae9e0a3fc4937e
secularism <1325041252@qq.com>
2022-04-24 14:17:20 +0800

    “主页的修改”

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,64 +8,96 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // swiper图片展示
-    swiperList: [{
-        img: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg',
-        time: '1921'
+    info: [{
+        start_time: '1921',
+        affair: [{
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          }
+        ],
+        name: '革命',
       },
       {
-        img: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big37006.jpg',
-        time: '1949'
+        start_time: '1949',
+        affair: [{
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          }
+        ],
+        name: '建设'
       },
       {
-        img: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg',
-        time: '2021'
+        start_time: '1978',
+        affair: [{
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '19xx',
+            name: 'xxx事件'
+          }
+        ],
+        name: '改革'
       },
+      {
+        start_time: '2012',
+        affair: [{
+            time: '20xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '20xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '20xx',
+            name: 'xxx事件'
+          },
+          {
+            time: '20xx',
+            name: 'xxx事件'
+          }
+        ],
+        name: '复兴',
+        end_time: "至今"
+      }
     ],
-    textInfoIndex:0,
-    img_index: [1, 2, 3, 4, 5, 6, 7, 8, 9],
     // tabBar默认主页活跃
     active: 0,
-    // swiper默认当前序号
-    currentSwiper: 0,
     show: false,
     IsLogin: false
-  },
-  switchSwiper(e) {
-    let swiperIndex = e.detail.current//获取图片swiper的轮播内容的index
-    this.setData({
-      textInfoIndex: swiperIndex //将图片swiper的index跟文字swiper的current属性关联起来
-    })
-  },
-  // swiper切换
-  /*
-  在bindchange的事件回调函数中改变currentIndex的值（当前所在滑块的索引），
-  会导致setData被不停的调用，
-  所以在改变currentIndex的值之前检测source是否是由于用户触摸引起的
-  */
-
-  swiperChange(event) {
-    // console.log(event.detail.current);
-    // console.log(event.detail.source);
-    if (event.detail.source == 'autoplay' || event.detail.source == 'touch') {
-      this.setData({
-        currentSwiper: event.detail.current
-      })
-    }
-  },
-  // 防止出现轮播图卡死现象最好使用bindanimationfinish方法切换当前下标的图片
-  animationChange(event) {
-    // console.log(event.detail.current);
-    this.setData({
-      currentSwiper: event.detail.current
-    })
-  },
-  // swiper点击
-  swiperClick() {
-    console.log(this.data.currentSwiper);
-    this.setData({
-      show: true
-    });
   },
   // tabBar切换
   onChange(event) {
@@ -180,5 +212,5 @@ Page({
     wx.navigateTo({
       url: '../question/question',
     })
-  }
+  },
 })
